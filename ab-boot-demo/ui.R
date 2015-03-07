12d46f47ba29f93f970048fcd0e16813ad2e8b74
Stephen Haptonstahl <srh@haptonstahl.org>
2015-03-07 13:22:53 -0500

    Linked to blog and source code

--- ab-boot-demo/ui.R
+++ ab-boot-demo/ui.R
@@ -44,5 +44,7 @@ fluidPage(
   helpText(HTML("<b>Vertical axis:</b> Represents the likelihood that the effect is that value. The higher the curve, the more likely the effect is there. (For those with a stats background, this is like a 'pdf' = probability density function.)")),
   helpText(HTML("<b>Minimal meaningful effect:</b> Represented by the vertical dotted lines, this is set to a value such that if the effect is smaller, then you would conclude that there is no meaningful difference between the cohorts. If most of the curve is between the dotted lines then there is no meaningful effect.")),
   helpText(HTML("<b>What if none of the percentages are close to 95% and it says I need a lot more data?</b> You could reconsider what constitutes a minimum meaningful effect.")),
-  helpText(HTML("<b>Access this page via:</b> http://polimath.shinyapps.io/ab-boot-demo"))
+  helpText(HTML("<b>Access this page via:</b> http://polimath.shinyapps.io/ab-boot-demo")),
+  helpText(HTML('<b>Read about it on my blog at:<b/> <a href="http://haptonstahl.org/polimath/?p=330" target="_blank">http://haptonstahl.org/polimath/</a>')),
+  helpText(HTML('<b>Access the source code via:</b> <a href="https://github.com/shaptonstahl/abtest/tree/master/ab-boot-demo" target="_blank">https://github.com/shaptonstahl/abtest/tree/master/ab-boot-demo</a>'))
 )
